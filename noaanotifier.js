07ede5bfbddd98a096f0f9697a51bd9c19849acc
John Casey <jdcasey@commonjava.org>
2021-01-25 23:59:23 -0600

    use openweathermap.org instead since NWS isn't reliable

--- noaanotifier.js
+++ noaanotifier.js
@@ -1,218 +1,38 @@
 /* Magic Mirror
- * Module: NOAACurrent
+ * Module: NOAANotifier
  * By John Casey https://github.com/jdcasey
- *
- * Based on Module: CurrentWeather,
- * By Michael Teeuw https://michaelteeuw.nl
  * MIT Licensed.
  */
 Module.register("noaanotifier", {
     // Default module config.
     defaults: {
+        // lat: <LATITUDE>,
+        // lon: <LONGITUDE>,
+        // appid: <API KEY>,
+        exclude: 'minutely',
         lat: config.lat,
         lon: config.lon,
-        station: null,
-        initialLoadDelay: 0, // 0 seconds delay
-        retryDelay: 2500,
+        units: config.units,
+        api_url: 'https://api.openweathermap.org/data/2.5/onecall',
 
-        apiBase: "https://api.weather.gov",
         updateInterval: 10 * 60 * 1000, // every 10 minutes
-    },
-
-    NOTIFICATION_GRIDPOINT_DATA: "NOAAWEATHER_GRIDPOINT_DATA",
-    NOTIFICATION_GRIDPOINT_CURRENT_DATA: "NOAAWEATHER_GRIDPOINT_CURRENT_DATA",
-    NOTIFICATION_CURRENT_DATA: "NOAAWEATHER_CURRENT_DATA",
-    NOTIFICATION_HOURLY_DATA: "NOAAWEATHER_HOURLY_DATA",
-    NOTIFICATION_FORECAST_DATA: "NOAAWEATHER_FORECAST_DATA",
-
-    // Define required scripts.
-    getScripts: function () {
-        return [];
-    },
-
-    // Define required scripts.
-    getStyles: function () {
-        return [];
-    },
-
-    // Define required translations.
-    getTranslations: function () {
-        // The translations for the default modules are defined in the core translation files.
-        // Therefor we can just return false. Otherwise we should have returned a dictionary.
-        // If you're trying to build your own module including translations, check out the documentation.
-        return false;
     },
 
     // Define start sequence.
     start: function () {
         Log.info("Starting module: " + this.name);
-
-        this.scheduleUpdate(this.config.initialLoadDelay);
     },
 
-    makeRequest: function(url, self){
-        return new Promise(function(resolve, reject){
-            var request = new XMLHttpRequest();
-            Log.log("Requesting from URL: " + url);
-            request.open("GET", url, true);
-
-            request.onload = function () {
-                if ( this.status === 200 ){
-                    // Log.log("URL: " + url + "\nhas response body:\n\n" + request.response);
-                    resolve(JSON.parse(request.response));
-                }
-                else{
-                    self.scheduleUpdate(self.loaded ? -1 : self.config.retryDelay);
-
-                    Log.error("Error calling " + url + ": " + this.status + " " + request.statusText );
-                    reject({
-                        status: this.status,
-                        statusText: request.statusText,
-                        err: "Response status: " + this.status,
-                    });
-                }
-            };
-
-            request.onerror = function(err){
-                self.scheduleUpdate(self.loaded ? -1 : self.config.retryDelay);
-                Log.error("Error calling " + url, err );
-                reject({
-                    status: this.status,
-                    statusText: request.statusText,
-                    err: err,
-                });
-            };
-
-            request.send();
-        });
+    notificationReceived: function(noti, payload, sender) {
+      if (noti == "DOM_OBJECTS_CREATED") {
+        this.sendSocketNotification("START", this.config)
+        return
+      }
     },
 
-    makeCurrentData: function(response){
-      Log.log("Creating current weather data object.");
-      const p = response.properties;
-
-      const feelsLike = p.windChill.value === null ?
-        p.heatIndex.value :
-        p.windChill.value;
-
-       return {
-        temp: p.temperature.value,
-        humidity: p.relativeHumidity.value,
-        feelsLike: feelsLike,
-        windSpeed: p.windSpeed.value,
-        windDeg: p.windDirection.value,
-        weatherIcon: p.icon,
-      };
-    },
-
-    updateWeatherInfo: function(){
-        // this.scheduleUpdate();
-
-        // Log.log("Looking up NOAA weather by lat/long");
-
-        const startUrl = this.config.apiBase + '/points/' + this.config.lat + "," + this.config.lon;
-        const self = this;
-        let officeData = {};
-
-        let pendingNotifications = {};
-
-        Log.log("Retrieving gridpoint information from: '" + startUrl + "'");
-        this.makeRequest(startUrl, self)
-        .then((response)=>{
-          officeData = response;
-          pendingNotifications[this.NOTIFICATION_GRIDPOINT_DATA] = response;
-        })
-        .then(()=>{
-          if ( self.config.station === null || self.config.station === 'undefined' ){
-            Log.log("Loading observation stations recorded at gridpoint.");
-            return self.makeRequest(officeData.properties.observationStations, self)
-            .then((obsResp)=>{
-              Log.log("Loading observation data from stations recorded at gridpoint.");
-              return self.makeRequest(obsResp.observationStations[0]  + "/observations/latest", self)
-              .then((resp)=>{
-                pendingNotifications[self.NOTIFICATION_CURRENT_DATA] = self.makeCurrentData(resp);
-              })
-              .catch((err) =>{
-                self.updateDom(self.config.animationSpeed);
-                Log.error("Failed to load current information for: " + obsResp.observationStations[0], err);
-              });
-            })
-            .catch((err) =>{
-              self.updateDom(self.config.animationSpeed);
-              Log.error("Failed to load observation stations for Lat/Lon: " + self.config.lat + "," + self.config.lon, err);
-            });
-          }
-          else{
-            const currentUrl = self.config.apiBase + '/stations/' + self.config.station + "/observations/latest";
-            Log.log("Loading observation data from configured station.");
-            return self.makeRequest(currentUrl, self)
-            .then((resp)=>{
-              pendingNotifications[self.NOTIFICATION_CURRENT_DATA] = self.makeCurrentData(resp);
-            })
-            .catch((err) =>{
-              self.updateDom(self.config.animationSpeed);
-              Log.error("Failed to load current information for Lat/Lon: " + self.config.lat + "," + self.config.lon, err);
-            });
-          }
-        })
-        .then(()=>{
-          Log.log("Got NWS office data.");
-
-          let urlToKey={};
-          urlToKey[officeData.properties.forecastGridData] = self.NOTIFICATION_GRIDPOINT_CURRENT_DATA,
-          urlToKey[officeData.properties.forecastHourly] = self.NOTIFICATION_HOURLY_DATA;
-          urlToKey[officeData.properties.forecast] = self.NOTIFICATION_FORECAST_DATA;
-
-          let promises = [];
-          Object.keys(urlToKey).forEach((u)=>{
-            promises.push(
-              self.makeRequest(u, self)
-              .then((response)=>{
-                const n = urlToKey[u];
-                Log.log("Loaded " + n + " from: " + u);
-                pendingNotifications[n] = response;
-              })
-              .catch((err) =>{
-                self.updateDom(self.config.animationSpeed);
-                Log.error("Failed to load " + n + " information for Lat/Lon: " + self.config.lat + "," + self.config.lon, err);
-              })
-            );
-          });
-
-          return Promise.all(promises);
-        })
-        .then((values)=>{
-          Log.log("All weather data loaded.");
-          Object.keys(pendingNotifications).forEach((n)=>{
-            Log.log("Sending notification: " + n);
-            self.sendNotification(n, pendingNotifications[n]);
-          })
-        })
-        .catch(function(err){
-            self.updateDom(self.config.animationSpeed);
-            Log.error("Failed to load weather information for Lat/Lon: " + self.config.lat + "," + self.config.lon, err);
-        });
-
-        this.scheduleUpdate();
-    },
-
-    /* scheduleUpdate()
-     * Schedule next update.
-     *
-     * argument delay number - Milliseconds before next update. If empty, this.config.updateInterval is used.
-     */
-    scheduleUpdate: function (delay) {
-        var nextLoad = this.config.updateInterval;
-        if (typeof delay !== "undefined" && delay >= 0) {
-            nextLoad = delay;
-        }
-
-        Log.log("Scheduling update for weather at " + nextLoad);
-
-        var self = this;
-        setTimeout(function () {
-            self.updateWeatherInfo();
-        }, nextLoad);
+    socketNotificationReceived: function(key, payload) {
+      this.sendSocketNotification("LOG", "Sending notification: " + key);
+      this.sendNotification(key, payload);
     },
 
 });
---
+++ node_helper.js
@@ -0,0 +1,231 @@
+/* Magic Mirror
+ * Module: NOAANotifier
+ * By John Casey https://github.com/jdcasey
+ * MIT Licensed.
+ */
+const axios = require("axios")
+const NodeHelper = require('node_helper')
+
+module.exports = NodeHelper.create({
+  start: function() {
+    console.log("weather notifier node_helper started.");
+  },
+
+  stop: function() {
+  },
+
+  socketNotificationReceived: function(noti, payload) {
+    switch(noti) {
+      case "START":
+        this.work(payload);
+        break;
+
+      case "LOG":
+        console.log(payload);
+        break;
+    }
+  },
+
+  work: function(config) {
+    this.startWeatherScan(config, this.updateWeather);
+  },
+
+  startWeatherScan: function(config, callback) {
+    console.log("weather notifier node_helper: starting weather scan.");
+    const url = config.api_url;
+
+    const self = this;
+    axios.get(url, {
+      params: {
+        lat: config.lat,
+        lon: config.lon,
+        appid: config.appid,
+        exclude: config.exclude,
+        units: config.units,
+      },
+    })
+    .then(res =>{
+      if(res.status == 200){
+        callback(res.data, config, self);
+      }
+    })
+    .catch(err =>{
+      if(err.response){
+        console.error("Error in response:", err.message);
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      }
+      else if (err.request){
+        console.error("Error in request:", err.message);
+        console.log(err.request);
+      }
+      else{
+        console.error('Error constructing request:', err.message);
+      }
+    })
+    .finally(()=>{
+      console.log("Weather will be refreshed again in " + (config.updateInterval / 1000) + " seconds.");
+      setTimeout( ()=>{
+          self.startWeatherScan(config, callback);
+        },
+        config.updateInterval
+      );
+    });
+  },
+
+  classifyWeather: function(weatherId, feelsLike, sunrise, sunset, dt){
+    switch(weatherId){
+      case 200:
+      case 210:
+      case 221:
+      case 230:
+      case 231:
+        return "wi-storm-showers";
+
+      case 201:
+      case 202:
+      case 211:
+      case 212:
+      case 232:
+        return "wi-thunderstorm";
+
+      case 300:
+      case 301:
+      case 310:
+      case 313:
+      case 321:
+        return "wi-showers";
+
+      case 302:
+      case 311:
+      case 312:
+      case 314:
+      case 321:
+        return "wi-rain";
+
+      case 500:
+      case 501:
+      case 520:
+      case 521:
+      case 531:
+        return "wi-sprinkle";
+
+      case 502:
+      case 503:
+      case 504:
+      case 522:
+        return "wi-raindrops";
+
+      case 511:
+      case 615:
+      case 616:
+        return "wi-rain-mix";
+
+      case 600:
+      case 601:
+      case 620:
+      case 621:
+        return "wi-snow";
+
+      case 602:
+      case 622:
+        return "wi-snowflake-cold";
+
+      case 611:
+      case 612:
+      case 613:
+        return "wi-sleet";
+
+      case 701:
+      case 741:
+        return "wi-fog";
+
+      case 711:
+      case 721:
+        return "wi-smoke";
+
+      case 731:
+      case 751:
+      case 761:
+      case 762:
+        return "wi-dust";
+
+      case 771:
+        return "wi-hail";
+
+      case 781:
+        return "wi-tornado";
+
+      case 800:
+        if ( dt < sunset ){
+          return feelsLike > 90 ? "wi-hot" : "wi-day-sunny";
+        }
+        else{
+          return feelsLike < 20 ? "wi-stars" : "wi-night-clear";
+        }
+
+      case 801:
+      case 802:
+        return dt < sunset ? "wi-day-cloudy" : "wi-night-alt-cloudy";
+
+      case 803:
+        return "wi-cloudy";
+
+      case 804:
+        return "wi-cloud";
+    }
+
+    return "wi-meteor";
+  },
+
+  classifyWindDeg: function(deg){
+    const classes = [
+      "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S",
+      "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"
+    ];
+
+    if ( deg < 11.25 ){
+      return "N";
+    }
+
+    const idx = Math.floor(deg / 22.5);
+    return classes[idx];
+  },
+
+  updateWeather: function(data, config, self) {
+    data.config = config;
+
+    if ( data.current !== undefined ){
+      data.current.weather.forEach((w)=>{
+        w.weatherClass = self.classifyWeather(w.id, data.current.feels_like, data.current.sunrise, data.current.sunset, data.current.dt);
+      });
+
+      data.current.windDirection = self.classifyWindDeg(data.current.wind_deg);
+    }
+
+    if ( data.hourly !== undefined ){
+      data.hourly.forEach((h)=>{
+        h.weather.forEach((w)=>{
+          w.weatherClass = self.classifyWeather(w.id, h.feels_like, data.current.sunrise, data.current.sunset, h.dt);
+        });
+
+        h.windDirection = self.classifyWindDeg(h.wind_deg);
+      });
+    }
+
+    if ( data.daily !== undefined ){
+      data.daily.forEach((d)=>{
+        d.weather.forEach((w)=>{
+          w.weatherClass = self.classifyWeather(w.id, d.feels_like.day, 0, 2, 1);
+        });
+
+        d.windDirection = self.classifyWindDeg(d.wind_deg);
+      });
+    }
+
+    console.log("weather notifier node_helper: notifying socket of refreshed weather.");
+    console.log(JSON.stringify(data.hourly, null, 2));
+    self.sendSocketNotification("WEATHER_REFRESHED", data);
+  },
+});
